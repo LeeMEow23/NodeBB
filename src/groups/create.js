3b51b45230ca988adbc68523be9ad485e3247298
Alexis W <alexis@alexiss-mbp-2.wifi.local.cmu.edu>
2024-08-30 13:47:32 -0400

    refactor code to reduce cognitive complexity

--- src/groups/create.js
+++ src/groups/create.js
@@ -6,29 +6,61 @@ const slugify = require('../slugify');
 const db = require('../database');
 
 module.exports = function (Groups) {
+	console.log('Alexis, now enter the refactored code');
 	Groups.create = async function (data) {
 		const isSystem = isSystemGroup(data);
 		const timestamp = data.timestamp || Date.now();
-		let disableJoinRequests = parseInt(data.disableJoinRequests, 10) === 1 ? 1 : 0;
-		if (data.name === 'administrators') {
-			disableJoinRequests = 1;
-		}
+		const disableJoinRequests = determineDisableJoinRequests(data);
 		const disableLeave = parseInt(data.disableLeave, 10) === 1 ? 1 : 0;
 		const isHidden = parseInt(data.hidden, 10) === 1;
 
 		Groups.validateGroupName(data.name);
 
+		await checkGroupExistence(data.name);
+
+		const groupData = buildGroupData(data, timestamp, disableJoinRequests, disableLeave, isHidden, isSystem);
+
+		await plugins.hooks.fire('filter:group.create', { group: groupData, data: data });
+
+		await saveGroupData(groupData, timestamp, data.ownerUid);
+
+		const finalGroupData = await Groups.getGroupData(groupData.name);
+		plugins.hooks.fire('action:group.create', { group: finalGroupData });
+		return finalGroupData;
+	};
+
+	function isSystemGroup(data) {
+		return data.system === true || parseInt(data.system, 10) === 1 ||
+			Groups.systemGroups.includes(data.name) ||
+			Groups.isPrivilegeGroup(data.name);
+	}
+
+	async function privilegeGroupExists(name) {
+		return Groups.isPrivilegeGroup(name) && await db.isSortedSetMember('groups:createtime', name);
+	}
+
+	async function checkGroupExistence(name) {
 		const [exists, privGroupExists] = await Promise.all([
-			meta.userOrGroupExists(data.name),
-			privilegeGroupExists(data.name),
+			meta.userOrGroupExists(name),
+			privilegeGroupExists(name),
 		]);
 		if (exists || privGroupExists) {
 			throw new Error('[[error:group-already-exists]]');
 		}
+	}
 
+	function determineDisableJoinRequests(data) {
+		if (data.name === 'administrators') {
+			return 1;
+		}
+		return parseInt(data.disableJoinRequests, 10) === 1 ? 1 : 0;
+	}
+
+	function buildGroupData(data, timestamp, disableJoinRequests, disableLeave, isHidden, isSystem) {
 		const memberCount = data.hasOwnProperty('ownerUid') ? 1 : 0;
 		const isPrivate = data.hasOwnProperty('private') && data.private !== undefined ? parseInt(data.private, 10) === 1 : true;
-		let groupData = {
+
+		return {
 			name: data.name,
 			slug: slugify(data.name),
 			createtime: timestamp,
@@ -42,18 +74,18 @@ module.exports = function (Groups) {
 			disableJoinRequests: disableJoinRequests,
 			disableLeave: disableLeave,
 		};
+	}
 
-		await plugins.hooks.fire('filter:group.create', { group: groupData, data: data });
-
+	async function saveGroupData(groupData, timestamp, ownerUid) {
 		await db.sortedSetAdd('groups:createtime', groupData.createtime, groupData.name);
 		await db.setObject(`group:${groupData.name}`, groupData);
 
-		if (data.hasOwnProperty('ownerUid')) {
-			await db.setAdd(`group:${groupData.name}:owners`, data.ownerUid);
-			await db.sortedSetAdd(`group:${groupData.name}:members`, timestamp, data.ownerUid);
+		if (ownerUid) {
+			await db.setAdd(`group:${groupData.name}:owners`, ownerUid);
+			await db.sortedSetAdd(`group:${groupData.name}:members`, timestamp, ownerUid);
 		}
 
-		if (!isHidden && !isSystem) {
+		if (!groupData.hidden && !groupData.system) {
 			await db.sortedSetAddBulk([
 				['groups:visible:createtime', timestamp, groupData.name],
 				['groups:visible:memberCount', groupData.memberCount, groupData.name],
@@ -64,20 +96,6 @@ module.exports = function (Groups) {
 		if (!Groups.isPrivilegeGroup(groupData.name)) {
 			await db.setObjectField('groupslug:groupname', groupData.slug, groupData.name);
 		}
-
-		groupData = await Groups.getGroupData(groupData.name);
-		plugins.hooks.fire('action:group.create', { group: groupData });
-		return groupData;
-	};
-
-	function isSystemGroup(data) {
-		return data.system === true || parseInt(data.system, 10) === 1 ||
-			Groups.systemGroups.includes(data.name) ||
-			Groups.isPrivilegeGroup(data.name);
-	}
-
-	async function privilegeGroupExists(name) {
-		return Groups.isPrivilegeGroup(name) && await db.isSortedSetMember('groups:createtime', name);
 	}
 
 	Groups.validateGroupName = function (name) {
